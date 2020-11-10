0c9592d59d9572cd9cd527c18354bc369fdebe14
keencho <seyoung050412@gmail.com>
2020-11-10 10:53:54 +0900

    infinite scroll query with total count

--- app/services/post.service.js
+++ app/services/post.service.js
@@ -6,20 +6,22 @@ export default {
     // CREATE
     create: async () => {
 
-        const post = new postSchema({
-            created: TimeUtils.getUTCDate(new Date()),
-            tag: ["DB"],
-            thumbnail: null,
-            path: "spring",
-            show: true,
-            title: "오늘의23",
-            summary: "test test 중입니다.",
-            contents: "test contents",
-            regexContents: "test regex Contents",
-            comments: null
-        });
+        for (let i = 0; i <= 50; i++) {
+            const post = new postSchema({
+                created: TimeUtils.getUTCDate(new Date()),
+                tag: ["DB"],
+                thumbnail: i % 2 === 0 ? "https://pfh.goodsflow.com/resources/image/2020-10-30/5755f651-79cb-4746-9f56-c564325665da.jpg" : null,
+                path: "spring",
+                show: true,
+                title: "2020-11-10: " + i + "번째",
+                summary: "test test 중입니다.",
+                contents: "test contents",
+                regexContents: "test regex Contents",
+                comments: null
+            });
 
-        await post.save();
+            await post.save();
+        }
     },
 
     // LIST BY QUERY
@@ -62,7 +64,7 @@ export default {
         return list;
     },
 
-    // LIST FOR INIFINITE SCROLL
+    // LIST FOR INFINITE SCROLL
     listInfiniteScroll: async (data) => {
         const start = Number(data.start);
         const limit = Number(data.limit);
@@ -73,12 +75,22 @@ export default {
         const tagMatch = StringUtils.hasText(tag) ? { tag : tag } : { tag : { $ne: null } } ;
         const dateMatch = isNaN(startDate.getTime()) ? { created: { $ne : null} } : { created: { $gte: startDate, $lte: endDate }};
 
-        return await postSchema.aggregate()
+        const result = await postSchema.aggregate()
             .match(tagMatch)
             .match(dateMatch)
             .sort("-created")
             .skip(start)
             .limit(limit);
+
+        const count = await postSchema
+            .countDocuments(tagMatch)
+            .countDocuments(dateMatch)
+            .exec();
+
+        return {
+            count: count,
+            rows: result
+        }
     },
 
     // LIST ALL
